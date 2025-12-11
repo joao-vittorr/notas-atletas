4d01edf2abb4e647fc2ac0292a2d0c8f8477b1ed
João Vitor <vitorribeiro9454@gmail.com>
2025-12-11 17:53:36 -0300

    Criando funcao principal do projeto

--- notas-atletas.js
+++ notas-atletas.js
@@ -16,3 +16,28 @@ let atletas = [
    notas: [10, 10, 10, 9, 9.5]
  }
 ];
+
+for (let i = 0; i < atletas.length; i++) {
+    let atleta = atletas[i];
+    // 1. Ordenar as notas
+    let notasOrdenadas = atleta.notas.sort(function(a, b) {
+        return a - b;
+    });
+    // 2. Remover a maior e a menor nota
+    let notasComputadas = notasOrdenadas.slice(1, 4);
+
+    // 3. Somar as notas restantes
+    let soma = 0;
+    notasComputadas.forEach(function(nota) {
+        soma = soma + nota;
+    });
+
+    // 4. Calcular a média
+    let media = soma / notasComputadas.length;
+
+    // 5. Apresentar o resultado
+    console.log("Atleta: " + atleta.nome);
+    console.log("Notas Obtidas: " + notasOrdenadas);
+    console.log("Média Válida: " + media);
+    console.log(""); // Linha em branco para separar os atletas
+}
